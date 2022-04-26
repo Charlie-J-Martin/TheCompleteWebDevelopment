b7628c01eb977fb1df5cc3896150c50854be6f29
Charlie Martin <charliejm7@gmail.com>
2022-04-26 16:56:58 +0100

    basic routes setup

--- TheCompleteWebDevelopment/backend-dev/authentication-and-security/app.js
+++ TheCompleteWebDevelopment/backend-dev/authentication-and-security/app.js
@@ -8,6 +8,18 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
 app.listen(3000, function (req, res) {
   console.log("Server sucessfully started on port 3000.");
 });
